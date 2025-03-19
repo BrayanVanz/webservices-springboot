7b230736401f95a93c035cb0576bb52663b11847
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2025-03-19 15:04:33 -0300

    Add Exception handler to delete

--- src/main/java/com/brayanvanz/webservices/services/UserService.java
+++ src/main/java/com/brayanvanz/webservices/services/UserService.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import com.brayanvanz.webservices.entities.User;
 import com.brayanvanz.webservices.repositories.UserRepository;
+import com.brayanvanz.webservices.services.exceptions.DatabaseException;
 import com.brayanvanz.webservices.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User user) {
