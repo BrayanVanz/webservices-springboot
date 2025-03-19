ab7186d82554bd5975e421587d46c66b0f05197b
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2025-03-19 14:07:00 -0300

    Enable User delete

--- src/main/java/com/brayanvanz/webservices/resources/UserResource.java
+++ src/main/java/com/brayanvanz/webservices/resources/UserResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.brayanvanz.webservices.entities.User;
 import com.brayanvanz.webservices.services.UserService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,5 +42,11 @@ public class UserResource {
         user = userService.insert(user);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
